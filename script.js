5a192f6f6d203fd44e23c89b2d1f959fbb98f82d
Jakub Ogrodnik <81594386+JakubOgrodnikPL@users.noreply.github.com>
2023-01-24 05:47:38 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+const promo = '20%';
+
+switch (promo){
+    case '10%': console.log(`Dziś mamy ${promo} zniżki`);
+    break;
+    case '20%': console.log(`Dziś mamy ${promo} zniżki`);
+    break;
+    case '30%': console.log(`Dziś mamy ${promo} zniżki`);
+    break;
+    default: console.log(`Dziś mamy inną promocję`);
+}
